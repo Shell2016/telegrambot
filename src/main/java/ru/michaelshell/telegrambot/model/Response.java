cc4d14b19f4fe5924345980f9c00d31d9131ef20
Shell2016 <bvg2003@bk.ru>
2024-08-14 15:01:38 +0300

    Unit tests: * TelegramClient config with @Bean * Added unit tests

--- src/main/java/ru/michaelshell/telegrambot/model/Response.java
+++ src/main/java/ru/michaelshell/telegrambot/model/Response.java
@@ -1,7 +1,9 @@
 package ru.michaelshell.telegrambot.model;
 
+import lombok.Builder;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboard;
 
+@Builder
 public record Response(Long chatId,
                        Integer messageId,
                        String message,
